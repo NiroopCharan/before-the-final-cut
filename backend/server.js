de2822e200ba57038a2cfb124cfae3bbfa048b14
Niroop Charan K S <niroopcharan@gmail.com>
2026-09-10 15:48:48 +0530

    Allow CORS from production

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
+app.use(cors()); // Allows requests from any origin including Vercel
 const multer = require("multer");
 const mongoose = require("mongoose");
 const fs = require("fs");
